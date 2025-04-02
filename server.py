21cab35cfb174b848455674b9efafb04d7cf6bf9
Shades <shades4355@gmail.com>
2025-04-02 15:09:14 -0400

    server ~ left comments for basic functionality outline (WIP) ~ imported sys for taking in arguments

---
+++ server.py
@@ -0,0 +1,28 @@
+import sys
+# use TCP
+
+# launch server
+
+# move database to server
+
+
+#########
+# Start #
+#########
+if __name__ == "__main__":
+    # server spin up
+        # pull ip and port from "sys.argv[1]" and "sys.agrv[2]"
+    # create empty database
+    # while True:
+        # try:
+            # listen for message
+            # receive message from a recipient
+                # validate message
+                # decode message
+                # if message is approved command - execute
+            # create rec_msg[bool, string]
+                # encode message
+            # send message back to same recipient
+        # catch KeyInterrupt:
+            # exit(0)
+    pass
