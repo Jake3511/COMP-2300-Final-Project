613862bf2082b2e12c15d6b85e46fbf69ca53324
Jake Shick <jake_shick@student.uml.edu>
2025-04-07 13:41:45 -0400

    Still testing Discord

--- secure_drop.py
+++ secure_drop.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
             # validate
             # decrypt message
         # print rec_msg[1] to command line
+        
         # send new message to Server
             # if msg == "exit", break
             # else:
