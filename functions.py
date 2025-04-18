ede236c7f967a47649e1e5633579a277dfadfbaf
Shades <shades4355@gmail.com>
2025-04-17 23:42:14 -0400

    alphabetized function and constants lists
    * removed unused lockout exception
    
    * tweaked login_client to not ask for name for returning user
    
    * (WIP) login_client returning user login broken

--- functions.py
+++ functions.py
@@ -9,19 +9,66 @@ SALT = bcrypt.gensalt() # used to generate a random number
 
 ACTION_LIST = ["help", "add", "list", "send", "exit"]
 
-USER = "User"
+
 CONTACTS = "Contacts"
 FULL_NAME = "Full_Name"
+LOGINS = "Logins"
 PASSWORD = "Password"
 TIME = "Time"
-LOGINS = "Logins"
+USER = "User"
 
 
+def actions_server(database:dict, command:int, username:str, data:list)->list:
+    match ACTION_LIST[command]:
+        case "add":
+            email, full_name = data
+            added_back = False
 
-# Optional "LockOut" Exception
-class LockOut(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+            for _, user_email, _ in database[USER][username][CONTACTS]:
+                if email == user_email:
+                    return [True, "Contact Already Added."]
+
+            try:
+                try:
+                    if database[USER][email]:
+                        added_back = True
+                        for _, user, added_back_bool in database[USER][email][CONTACTS]:
+                            if email == user:
+                                database[USER][email][CONTACTS][user][2] = True
+                except KeyError:
+                    added_back = False
+
+                database[USER][username][CONTACTS].append([full_name, email, added_back])
+
+            except KeyError:
+                database[USER][username][CONTACTS] = []
+                database[USER][username][CONTACTS].append([full_name, email, added_back])
+
+            return [True, "Contact Added."]
+
+        case "list": # TODO: add online test
+            try:
+                contacts = database[USER][username][CONTACTS]
+                friend = False
+                msg = str()
+                for contact, email, added_back_bool in contacts:
+                    if added_back_bool == True:
+                        friend = True
+                        msg.append(f"\t* {contact} <{email}>")
+                    if not contacts[-1] == [contact, email, added_back_bool]:
+                        msg.append("\n")
+                    return msg
+                if friend == False:
+                    return "\tNo Contacts Added You Back"
+            except KeyError:
+                return "\tContact List Is Empty."
+
+        case "send": # TODO: flesh out
+            pass
+
+
+def comp_str(p1:str, p2:str)->bool:
+    return p1 == p2
 
 
 def get_hashed_password(password:str)->str:
@@ -35,39 +82,20 @@ def get_hashed_password(password:str)->str:
     # return password in json formatted string (was giving me an error when tried to save binary string in json format)
 
 
-def comp_str(p1:str, p2:str)->bool:
-    return p1 == p2
+def get_name_and_email(indent="")->list:
+    # Get Full Name
+    full_name = input(f"{indent}Enter Full Name: ")
+    # Get username/email
+    # TODO: validate password via regex
+    username = input(f"{indent}Enter Email Address: ").lower()
+
+    return full_name, username
 
 
 def get_password(msg:str)->str:
     # TODO: add password strength validation?
     password = pwinput.pwinput(prompt=msg, mask="*")
     return get_hashed_password(password)
-
-
-def new_user()->list:
-    '''Creates a new user and adds them to the Database'''
-    full_name, username = get_name_and_email()
-
-    passwd1 = ""
-    passwd2 = "a"
-
-    n = 0
-    while not passwd1 == passwd2:
-        n += 1
-        passwd1 = get_password("Enter Password: ")
-        passwd2 = get_password("Re-enter Password: ")
-
-        if not passwd1 == passwd2:
-            print("\nPassword Mis-match.")
-            print("Try Again.\n")
-
-        if n > 5:
-            print("Password Attempts Exceeded.")
-            return [False, None, None, None]
-
-    print("\nPasswords Match.")
-    return [True, username, full_name, passwd1]
 
 
 def login_client()->list:
@@ -82,7 +110,8 @@ def login_client()->list:
         if new:
             success, email, full_name, password = new_user()
         else:
-            full_name, email = get_name_and_email()
+            email = input("Enter Email Address: ")
+            full_name = None
             password = get_password()
 
         if success:
@@ -139,60 +168,26 @@ def login_server(database:dict, new:bool, user:list, password:str)->list:
         return [False, "Log In Failed."]
 
 
-def actions_server(database:dict, command:int, username:str, data:list)->list:
-    match ACTION_LIST[command]:
-        case "add":
-            email, full_name = data
-            added_back = False
+def new_user()->list:
+    '''Creates a new user and adds them to the Database'''
+    full_name, username = get_name_and_email()
 
-            for _, user_email, _ in database[USER][username][CONTACTS]:
-                if email == user_email:
-                    return [True, "Contact Already Added."]
+    passwd1 = ""
+    passwd2 = "a"
 
-            try:
-                try:
-                    if database[USER][email]:
-                        added_back = True
-                        for _, user, added_back_bool in database[USER][email][CONTACTS]:
-                            if email == user:
-                                database[USER][email][CONTACTS][user][2] = True
-                except KeyError:
-                    added_back = False
+    n = 0
+    while not passwd1 == passwd2:
+        n += 1
+        passwd1 = get_password("Enter Password: ")
+        passwd2 = get_password("Re-enter Password: ")
 
-                database[USER][username][CONTACTS].append([full_name, email, added_back])
+        if not passwd1 == passwd2:
+            print("\nPassword Mis-match.")
+            print("Try Again.\n")
 
-            except KeyError:
-                database[USER][username][CONTACTS] = []
-                database[USER][username][CONTACTS].append([full_name, email, added_back])
+        if n > 5:
+            print("Password Attempts Exceeded.")
+            return [False, None, None, None]
 
-            return [True, "Contact Added."]
-
-        case "list": # TODO: add online test
-            try:
-                contacts = database[USER][username][CONTACTS]
-                friend = False
-                msg = str()
-                for contact, email, added_back_bool in contacts:
-                    if added_back_bool == True:
-                        friend = True
-                        msg.append(f"\t* {contact} <{email}>")
-                    if not contacts[-1] == [contact, email, added_back_bool]:
-                        msg.append("\n")
-                    return msg
-                if friend == False:
-                    return "\tNo Contacts Added You Back"
-            except KeyError:
-                return "\tContact List Is Empty."
-
-        case "send": # TODO: flesh out
-            pass
-
-
-def get_name_and_email(indent="")->list:
-    # Get Full Name
-    full_name = input(f"{indent}Enter Full Name: ")
-    # Get username/email
-    # TODO: validate password via regex
-    username = input(f"{indent}Enter Email Address: ").lower()
-
-    return full_name, username
+    print("\nPasswords Match.")
+    return [True, username, full_name, passwd1]
